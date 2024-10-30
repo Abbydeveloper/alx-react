ed4bf1bc645f97f2831233ae3fa15f89ecc765a0
Adeboga Abigail <abbyboga@gmail.com>
2024-10-30 08:54:49 +0000

    Fix src/index.js file

--- 0x00-Webpack/task_0/src/index.js
+++ 0x00-Webpack/task_0/src/index.js
@@ -1,11 +1,7 @@
 import $ from 'jquery';
 
 $(document).ready(function () {
-  ${body}.append(
-    "
-      <p>Holberton Dashboard</p>
-      <p>Dashboard data for the students</p>
-      <p>Copyright - Hoblerton School</p>
-    "
-  );
+  $(body).append("<p>Holberton Dashboard</p>");
+  $(body).append("<p>Dashboard data for the students</p>");
+  $(body).append("<p>Copyright - Hoblerton School</p>");
 });
