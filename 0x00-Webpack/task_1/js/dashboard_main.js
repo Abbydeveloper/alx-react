8bb790ff8e464709b9edfc6b92eb7bb8ee4486ff
Adeboga Abigail <abbyboga@gmail.com>
2024-10-30 09:55:15 +0000

    Using jQuery

---
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -0,0 +1,7 @@
+import $ from 'jquery';
+
+$('body').append('<p>Holberton Dashboard</p>');
+$('body').append('<p>Dashboard data for the students</p>');
+$('body').append('<p>Click here to get started</p>');
+$('body').append('<p id="count"></p>');
+$('body').append('<p>Copyright - Holberton School</p>');
